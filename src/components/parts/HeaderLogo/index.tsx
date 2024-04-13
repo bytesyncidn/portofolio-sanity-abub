f51bde9fcd3768857b949c2b04d67d1460aefc7f
rusdaii <rusdaii.html@gmail.com>
2024-04-13 21:20:01 +0700

    feat: add logo media

--- src/components/parts/HeaderLogo/index.tsx
+++ src/components/parts/HeaderLogo/index.tsx
@@ -1,15 +1,28 @@
+import { draftMode } from 'next/headers';
 import Image from 'next/image';
 
-const logoUrl = 'https://placehold.co/400x400.png';
+import { readToken } from '~/lib/sanity/sanity.api';
+import { getClient } from '~/lib/sanity/sanity.client';
+import { urlForImage } from '~/lib/sanity/sanity.image';
+import { getLogo } from '~/lib/sanity/sanity.queries';
 
-const HeaderLogo = () => {
+const placeholderUrl = 'https://placehold.co/400x400.png';
+
+const HeaderLogo = async () => {
+  const { isEnabled: isEnabledDraftMode } = draftMode();
+  const client = getClient(isEnabledDraftMode ? { token: readToken } : undefined);
+
+  const logo = await getLogo(client);
+
+  const logoUrl = urlForImage(logo[0].logoImage)?.url() ?? placeholderUrl;
+
   return (
     <Image
       src={logoUrl}
       alt="Tengen Hekaru"
       width={100}
       height={100}
-      className="object-cover rounded-full"
+      className="object-cover w-20 h-20 rounded-full"
     />
   );
 };
